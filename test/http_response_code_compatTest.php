6493251cf5504823e5cebcfe566b0d97c342d0ef
Martin Lindhe <martin@ubique.se>
2015-03-24 11:22:28 +0100

    packaging updates

--- test/http_response_code_compatTest.php
+++ test/http_response_code_compatTest.php
@@ -1,16 +1,24 @@
 <?php
 
-// NOTE this needs to be tested on < php 5.3 to make any sense
-
-require __DIR__.'./../lib/http_response_code_compat.php'; // XXX auto loading
-
 class http_response_code_compatTest extends \PHPUnit_Framework_TestCase
 {
-    function test1()
+    function isVersionTooNew()
     {
-        // FIXME xdebug_get_headers() dont seem to return the status headers
-        http_response_code(404);
+        $version = explode('.', PHP_VERSION);
 
-        $this->assertTrue(in_array('Status: 404 Not Found', xdebug_get_headers()));
+        if (($version[0] == 5 && $version[1] >= 4) || $version[0] > 5) {
+            return true;
+        }
+
+        return false;
+    }
+
+    function testReturnCode()
+    {
+        if ($this->isVersionTooNew()) {
+            $this->markTestSkipped('Only runs on php < 5.4');
+        }
+
+        $this->assertEquals(404, http_response_code(404));
     }
 }
